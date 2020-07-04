4a4828865fddf7e663b489b689f64e98cc8d4894
vikas kumar <viomjeet@gmail.com>
2020-07-04 14:16:24 +0530

    Updates

---
+++ precache-manifest.c520706c70f402cd490d9a6f7460ced4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fecce085781e15d2d2459b5dd383c764",
+    "url": "/redux_2/index.html"
+  },
+  {
+    "revision": "318235e6897101618dcc",
+    "url": "/redux_2/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8f3119f516a48d780e71",
+    "url": "/redux_2/static/css/main.15d74f7d.chunk.css"
+  },
+  {
+    "revision": "318235e6897101618dcc",
+    "url": "/redux_2/static/js/2.0f82c22b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux_2/static/js/2.0f82c22b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f3119f516a48d780e71",
+    "url": "/redux_2/static/js/main.f721c73d.chunk.js"
+  },
+  {
+    "revision": "95d488659c603bcf825e",
+    "url": "/redux_2/static/js/runtime-main.6dd58f7b.js"
+  }
+]);
