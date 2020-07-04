fe3f44e21aced972fd3c52ab0f7fb07fd2b5096d
vikas kumar <viomjeet@gmail.com>
2020-07-04 14:25:52 +0530

    first commit

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,4 +1,4 @@
 import { createStore } from 'redux';
 import todosList from './reducer/reducers';
-const store = createStore(todosList)
+const store = createStore(todosList, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 export default store;
